5ad1bfc2124088d221e5f981c1d251c19e2dcdf2
Gowtham-314 <gowdagowtham7930@gmail.com>
2026-01-03 15:17:33 +0530

    update to modules

---
+++ E-cart-system/main.py
@@ -0,0 +1,7 @@
+""""<!--- E-Commerce Cart System
+    This program allows users to add items to a shopping cart, view the cart contents,
+    Systemed by Kruthik BT , Gowtham Gowda  C B , Akash B V , Rohith S J
+    @Coding_group--->"""
+
+
+
